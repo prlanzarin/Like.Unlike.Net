887e3ddb90bcba53dc96c4ee1ac3c6a87ba4336a
prlanzarin <prlanzarin@inf.ufrgs.br>
2013-11-07 01:58:34 -0200

    Main chama função para leitura de arquivo

--- main.c
+++ main.c
@@ -1,1 +1,12 @@
-//main
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lunet.h"
+
+int main()
+{
+  FILE *input;
+  printf("Resultado do arquivo de entrada: \n");
+  getOperation(input);
+  return 0;
+}
