6b64058074e15b54c24ce325ffe1aefa3986470a
Luis <luis@N46VM.(none)>
2013-11-07 23:42:51 -0200

    arquivo velho

--- structuser.h
+++ structuser.h
@@ -1,3 +1,17 @@
-define struct user {
+// estrutura do usuário
 
-} typedef struct user User;
+struct user {
+
+    char nome[50];
+    struct friendList *like, *unlinke;
+
+};
+typedef struct user User;
+
+struct friendList {
+    User *pUser;
+    int appearances;
+    struct friendList *next;
+};
+typedef struct friendList FriendList;
+
